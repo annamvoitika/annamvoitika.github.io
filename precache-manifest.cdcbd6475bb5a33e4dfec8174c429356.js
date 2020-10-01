f2b47ef078c47097f35905dd07f92b72e9559a45
Anna <t-avoitika@hotels.com>
2020-10-01 14:10:15 +0100

    Updates

---
+++ precache-manifest.cdcbd6475bb5a33e4dfec8174c429356.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504e9036360f6502140388e7ce8fead0",
+    "url": "/annamvoitika.github.io/index.html"
+  },
+  {
+    "revision": "f52d7d3998e5999261df",
+    "url": "/annamvoitika.github.io/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c26dd0715053ee3b0b8c",
+    "url": "/annamvoitika.github.io/static/css/main.8fe2226e.chunk.css"
+  },
+  {
+    "revision": "f52d7d3998e5999261df",
+    "url": "/annamvoitika.github.io/static/js/2.79e637ff.chunk.js"
+  },
+  {
+    "revision": "7631940ea5cc44ddfa369d449caef6c4",
+    "url": "/annamvoitika.github.io/static/js/2.79e637ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26dd0715053ee3b0b8c",
+    "url": "/annamvoitika.github.io/static/js/main.8d5a18fa.chunk.js"
+  },
+  {
+    "revision": "2105af0281c638495e31",
+    "url": "/annamvoitika.github.io/static/js/runtime-main.ec10730f.js"
+  },
+  {
+    "revision": "f34f4bd4196673fad98ce42926910bb5",
+    "url": "/annamvoitika.github.io/static/media/anna3.f34f4bd4.jpg"
+  },
+  {
+    "revision": "e2b43518eb03c8e545c1e1c7eb613a7f",
+    "url": "/annamvoitika.github.io/static/media/article.e2b43518.png"
+  },
+  {
+    "revision": "432f9db709409780fed90449aac6c799",
+    "url": "/annamvoitika.github.io/static/media/chat.432f9db7.png"
+  },
+  {
+    "revision": "210ad387b1f7560319f41a541fac5ba8",
+    "url": "/annamvoitika.github.io/static/media/main.210ad387.png"
+  },
+  {
+    "revision": "650d198bac44a61abd2a4d0b0c798ac6",
+    "url": "/annamvoitika.github.io/static/media/plentyofdish.650d198b.png"
+  },
+  {
+    "revision": "dec5a402b7000cec8bf1ab3e97f4b32d",
+    "url": "/annamvoitika.github.io/static/media/soon.dec5a402.png"
+  },
+  {
+    "revision": "a2015ffb6959873e1029c5ec41c5fc2e",
+    "url": "/annamvoitika.github.io/static/media/trello.a2015ffb.png"
+  }
+]);
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-it("renders the main page", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Anna/i);
-  expect(linkElement).toBeInTheDocument();
-});
