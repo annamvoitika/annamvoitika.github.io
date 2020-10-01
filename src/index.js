3c43a64f50c8a60d2ba122d3ce4154694061b687
Anna <t-avoitika@hotels.com>
2020-10-01 14:01:54 +0100

    Testing app

--- src/index.js
+++ src/index.js
@@ -9,17 +9,6 @@ import * as serviceWorker from "./serviceWorker";
 import PlentyOfDish from "./PlentyOfDish.js";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/plentyofdish" component={PlentyOfDish} />
-    </Switch>
-  </BrowserRouter>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
