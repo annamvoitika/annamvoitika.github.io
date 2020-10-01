02fe58fa2e5345603577a93fc7b74af4c558cf4b
Anna <t-avoitika@hotels.com>
2020-10-01 13:37:46 +0100

    Updates

---
+++ precache-manifest.92a87c8e04013b6aeeff65948c5b59dd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91632980904fe7a97370098621d39e68",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/index.html"
+  },
+  {
+    "revision": "d5c3ea5f7909e5aede31",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "d8f14c8bc591274d480e",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/css/main.8fe2226e.chunk.css"
+  },
+  {
+    "revision": "d5c3ea5f7909e5aede31",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/js/2.8412b463.chunk.js"
+  },
+  {
+    "revision": "7631940ea5cc44ddfa369d449caef6c4",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/js/2.8412b463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f14c8bc591274d480e",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/js/main.5e1189ba.chunk.js"
+  },
+  {
+    "revision": "1d07280ce0c0f67adda7",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/js/runtime-main.5b9fdd44.js"
+  },
+  {
+    "revision": "f34f4bd4196673fad98ce42926910bb5",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/media/anna3.f34f4bd4.jpg"
+  },
+  {
+    "revision": "e2b43518eb03c8e545c1e1c7eb613a7f",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/media/article.e2b43518.png"
+  },
+  {
+    "revision": "432f9db709409780fed90449aac6c799",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/media/chat.432f9db7.png"
+  },
+  {
+    "revision": "210ad387b1f7560319f41a541fac5ba8",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/media/main.210ad387.png"
+  },
+  {
+    "revision": "650d198bac44a61abd2a4d0b0c798ac6",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/media/plentyofdish.650d198b.png"
+  },
+  {
+    "revision": "dec5a402b7000cec8bf1ab3e97f4b32d",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/media/soon.dec5a402.png"
+  },
+  {
+    "revision": "a2015ffb6959873e1029c5ec41c5fc2e",
+    "url": "/annamvoitika.github.io/annamvoitika.github.io/static/media/trello.a2015ffb.png"
+  }
+]);
